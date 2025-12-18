fd0d255d072e61e9b21b0da89eb7b33b0b509536
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-12-17 21:05:29 -0300

    fix: add special category and ruby rarity to medal enums

--- src/models/medal.js
+++ src/models/medal.js
@@ -26,7 +26,7 @@ module.exports = (sequelize) => {
             allowNull: false
         },
         category: {
-            type: DataTypes.ENUM('patrimony', 'investment', 'saving', 'consistency', 'social', 'milestone'),
+            type: DataTypes.ENUM('patrimony', 'investment', 'saving', 'consistency', 'social', 'milestone', 'special'),
             allowNull: false
         },
         icon: {
@@ -34,7 +34,7 @@ module.exports = (sequelize) => {
             defaultValue: 'trophy'
         },
         rarity: {
-            type: DataTypes.ENUM('bronze', 'silver', 'gold', 'platinum', 'diamond'),
+            type: DataTypes.ENUM('bronze', 'silver', 'gold', 'platinum', 'diamond', 'ruby'),
             defaultValue: 'bronze'
         },
         requirement: {
