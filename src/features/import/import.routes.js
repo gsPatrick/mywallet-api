23b0ecf053be82495874497f578687b7948e42b0
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-01-18 15:56:39 -0300

    Fix: Import authMiddleware correctly in import routes

--- src/features/import/import.routes.js
+++ src/features/import/import.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const importController = require('./import.controller');
-const { protect } = require('../../middlewares/authMiddleware');
+const { authMiddleware: protect } = require('../../middlewares/authMiddleware');
 
 router.post('/ofx/preview', protect, importController.previewOFX);
 router.post('/ofx/confirm', protect, importController.confirmImport);
